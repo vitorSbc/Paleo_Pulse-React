b9dcaf0f1dc630eee711dff7e4b16a1c77daa7db
Vitor <vitorhugoleandro2015@gmail.com>
2024-11-21 17:39:59 -0300

    Implementando sistema de bluer nas noticias em destaque

--- src/components/SectionBig/index.jsx
+++ src/components/SectionBig/index.jsx
@@ -2,10 +2,13 @@ import News from "./sources/News";
 import "./styles.css";
 
 export default function SectionBig() {
+  
+  
+
   return (
     <>
         <div className="Container">
-            <News news="newsFirst"/>
+            <News news="newsFirst" />
             <News news="newsMiddle"/>
             <News news="newsEnd"/>
         </div>
--- src/components/SectionBig/sources/News.jsx
+++ src/components/SectionBig/sources/News.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import "./news.css"
 
 export default function News(props){
     const  news  = props.news;
     const backgroundImg = news 
+    const [blur, setBlur] = useState(true);
+
     
-    
+
     return(
        <>
-        <div className={`Teste ${backgroundImg}`}></div>
+        <div className={`Teste ${backgroundImg}`} onMouseEnter={handling_enter} onMouseOut={handling_out}>
+            <div className={blur}></div>
+        </div>
+        
        </>
 )
 }
